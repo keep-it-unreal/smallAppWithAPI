4fe158839297fd902c8350b2ca1856ab25466b2d
keep-it-unreal <112315703+keep-it-unreal@users.noreply.github.com>
2022-11-05 17:47:53 +0300

    Update ConnectManager.java

--- src/main/java/fileManager/ConnectManager.java
+++ src/main/java/fileManager/ConnectManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public final class ConnectManager {
 
     private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "Zil131";
+    private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/TreeStr";
 
     private ConnectManager(){
